c34f9af7788fe41be12b0d8a94373296f7a8f4dd
Nilton Heck <nilton@chainengine.xyz>
2022-09-28 11:43:16 -0300

    feat: add listing support

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { PlayerService } from './players/player.service';
 import { GameService } from './games/game.service';
 import { NftService } from './nft/nft.service';
 import { WalletService } from './wallet/wallet.service';
-export { ChainEngineSdk, PlayerService, GameService, NftService, WalletService };
+import { ListingService } from './marketplace/listing/listing.service';
+export { ChainEngineSdk, PlayerService, GameService, NftService, WalletService, ListingService };
